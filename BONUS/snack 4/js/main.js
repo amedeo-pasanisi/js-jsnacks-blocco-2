1d5296ccd18dbb77771da40258e7896c3563da24
amedeo <amedeopasanisi97@gmail.com>
2021-07-22 10:04:35 +0200

    generalizzazione snack 4

--- BONUS/snack 4/js/main.js
+++ BONUS/snack 4/js/main.js
@@ -1,16 +1,22 @@
 // Crea due array che hanno un numero di elementi diversi.
 // Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 
-var arrayMaggiore = ["elemento", "elemento", "elemento", "elemento", "elemento"]; // 5 elementi
-console.log("numero elementi all'interno dell'array maggiore: " + arrayMaggiore.length);
+var array1 = ["elemento", "elemento", "elemento", "elemento", "elemento"]; // 5 elementi
+console.log("numero elementi all'interno dell'array 1: " + array1.length);
 
-var arrayMinore = ["elemento", "elemento", "elemento"]; // 3 elementi
-console.log("numero elementi all'interno dell'array minore: " + arrayMinore.length);
+var array2 = ["elemento", "elemento", "elemento"]; // 3 elementi
+console.log("numero elementi all'interno dell'array 2: " + array2.length);
 
-console.log("elementi da aggiungere all'array minore: " + (arrayMaggiore.length - arrayMinore.length));
+console.log("elementi da aggiungere all'array 2: " + (array1.length - array2.length));
 
-while (arrayMinore.length != arrayMaggiore.length) {
-    arrayMinore.push ("elemento");
+if (array1.length > array2.length) {
+    var minore = array2
+} else if (array1.length < array2.length) {
+    minore = array1
+}
+
+while (array2.length != array1.length) {
+    minore.push ("elemento");
     console.log ("+1 elemento");
 }
 
